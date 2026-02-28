9ed556487b82ef4e9f2f1b35ac9ada7ccb645f14
Natig <natigagharzayev@gmail.com>
2026-02-28 13:25:17 +0200

    feat: add genre controller

--- backend/src/genres/genres.controller.ts
+++ backend/src/genres/genres.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { GenresService } from './genres.service';
+import { Genre } from './genre.types';
 
 @Controller('genres')
-export class GenresController {}
+export class GenresController {
+    constructor(private readonly genresService: GenresService) { }
+
+    @Get()
+    getGenres(): Promise<Genre[]> {
+        return this.genresService.getGenres();
+    }
+}
