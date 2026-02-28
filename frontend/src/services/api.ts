fae0cfa0f0699ea553c48aa1be18edd98c121d58
Natig <natigagharzayev@gmail.com>
2026-02-28 19:52:05 +0200

    refactor: increase readabilty

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -9,7 +9,10 @@ export const fetchMovies = (params: {
     search?: string
     genre?: number
     page?: number
-}): Promise<MovieListResponse> => api.get('/movies', { params }).then((res) => res.data)
+}): Promise<MovieListResponse> => {
+    return api.get('/movies', { params }).then((res) => res.data)
+}
 
-export const fetchGenres = (): Promise<Genre[]> =>
-    api.get('/genres').then((res) => res.data)
+export const fetchGenres = (): Promise<Genre[]> => {
+    return api.get('/genres').then((res) => res.data)
+}
