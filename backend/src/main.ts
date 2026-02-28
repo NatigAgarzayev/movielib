bdcdb9bf4640f3f3feeaea717e2cd255256409a3
Natig <natigagharzayev@gmail.com>
2026-02-28 13:41:22 +0200

    feat: add global error handling

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { GlobalExceptionFilter } from './common/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.enableCors({
     origin: ['http://localhost:5173', 'http://localhost:3000'],
