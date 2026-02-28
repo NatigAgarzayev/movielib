f6508e76676372f7a65a6ba99b8a5f4ebeb29093
Natig <natigagharzayev@gmail.com>
2026-02-28 13:15:29 +0200

    feat: add movie controller

--- backend/src/movies/movies.controller.ts
+++ backend/src/movies/movies.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
+import { MoviesService } from './movies.service';
+import { MovieListResponse } from './movie.types';
 
 @Controller('movies')
-export class MoviesController {}
+export class MoviesController {
+    constructor(private readonly moviesService: MoviesService) { }
+
+    @Get()
+    getMovies(
+        @Query('search') search?: string,
+        @Query('page') page?: string,
+    ): Promise<MovieListResponse> {
+        const pageNumber = page ? parseInt(page, 10) : 1;
+
+        if (search) {
+            return this.moviesService.searchMovies(search, pageNumber);
+        }
+
+        return this.moviesService.getPopularMovies(pageNumber);
+    }
+}
