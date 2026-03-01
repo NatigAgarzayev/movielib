eb9c0cf4479be5b43b4ee20427edb95f62d648b4
Natig <natigagharzayev@gmail.com>
2026-03-01 21:23:13 +0200

    fix: add CORS frontend url

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.useGlobalFilters(new GlobalExceptionFilter());
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://localhost:3000'],
+    origin: ['http://localhost:5173', 'http://localhost:3000', 'https://movielib-eosin.vercel.app/'],
     methods: ['GET'],
   });
 
