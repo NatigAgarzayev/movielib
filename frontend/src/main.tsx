76298825dc5035dc628e3ef98edba077c9dc8eb5
Natig <natigagharzayev@gmail.com>
2026-03-01 19:29:18 +0200

    feat: scroll to the top in every reload

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,11 @@ import { store } from './store';
 import App from './App.tsx'
 import { Provider } from 'react-redux'
 
+if ('scrollRestoration' in history) {
+  history.scrollRestoration = 'manual'
+}
+window.scrollTo(0, 0)
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
